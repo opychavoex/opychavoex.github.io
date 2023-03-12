4d2a158e30448f438cff08b4cd0834a17efab372
opychavoex <122548558+opychavoex@users.noreply.github.com>
2023-03-12 22:59:54 +0700

    Add files via upload

---
+++ iklan.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'adf7833a39a5f9cf4cab488902dd4c0b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://floodingdaredsanctuary.com/adf7833a39a5f9cf4cab488902dd4c0b/invoke.js"></scr' + 'ipt>');
